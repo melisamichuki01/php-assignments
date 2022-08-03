6e07705aa22686d7bae449ab79f762aaaaa48014
Melisa Michuki <61727167+melisamichuki01@users.noreply.github.com>
2022-08-03 14:39:15 +0300

    Add files via upload

---
+++ conditionals.php
@@ -0,0 +1,49 @@
+<?php
+//Yesterday we covered if & switch statements.
+//These are conditional statements that are used to execute code based on a certain condition
+// As a way of practicing, we will work on a few exercises
+
+//Remember the syntax is as follows:
+//if (condition) {
+    //code to be executed if condition is true
+//};elseif/else(condition) {
+    //code to be executed if condition is false
+//};else {
+    //code to be executed if all conditions are false
+//}
+
+//switch statement syntax is:
+//switch(expression) {
+    //case (condition):
+        //code to be executed if condition is true
+        //break;
+    //case (condition):
+        //code to be executed if condition is true
+        //break;
+    //default:
+     //code to be executed if condition is true
+     //break;
+//}
+
+//Exercises
+
+//1.An electrical company bills customers based on the amount of electricity they use.
+//There are different tariffs for different amounts of electricity.
+//The company has a bill for 0-50 kWh  - 100sh per kWh, a bill for 51-100 kWh-150sh per kWh,
+//a bill for 101-200 kWh -200sh per kWh, 
+//and a bill for 201-300 kWh - 300sh per kWh.
+// using operators and conditional statements ,bill a customer who has a consumption of 250kWh
+// create one for if and switch
+
+//2.An company has called in an audit firm to  audit their records
+// Most employees earn between 50,000 - 500,000
+// create a salary groupings for different pay grades 
+// using if and switch case
+// if a value greater than 500000  is entered 
+// output that there is a  discrepancy
+
+//
+
+
+
+?>
