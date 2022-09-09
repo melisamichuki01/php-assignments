bbbf0f10e7eed6953b7ad61ae90636d16ee464af
Melisa Michuki <61727167+melisamichuki01@users.noreply.github.com>
2022-09-09 13:54:42 +0300

    Update index.php

--- logiregdemo/index.php
+++ logiregdemo/index.php
@@ -1,56 +1,66 @@
 <?php
 @include 'config\dbcon.php';
-if(isset($_POST['login'])) {
-    $username = mysqli_real_escape_string($con,$_POST['UserName']);
-    $password = mysqli_real_escape_string($con,$_POST['Password']);
-    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
-    $result = mysqli_query($con, $sql);
-    if(mysqli_num_rows($result) > 0) {
-        echo "Login Successful";
-    } else {
-        echo "Login Failed";
-    }
-}
-
 ?>
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Page</title>
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <div class="card mt-5">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
                     <div class="card-header">
-                        <h3 class="text-center">Login</h3>
+                      <h4>User Details 
+                      <a href="create_user.php" class="btn btn-primary">Create New User</a>
+                      </h4>  
                     </div>
                     <div class="card-body">
-                        <form action="" method="post">
-                            <div class="mb-3">
-                                <label for="username">Username</label>
-                                <input type="text" name="Username" id="username" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="password">Password</label>
-                                <input type="password" name="Password" id="password" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" class="btn btn-primary" value="login" name="login" >Login</button>
-                            </div>
-                        </form>
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Password</th>
+                            <th scope="col">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                            $sql = "SELECT * FROM users";
+                            $result = mysqli_query($con,$sql);
+                            if(mysqli_num_rows($result)>0){
+                              foreach($result as $row){
+                                ?>
+                                <tr>
+                                  <td><?=$row['id']?></td>
+                                  <td><?=$row['username']?></td>
+                                  <td><?=$row['Email']?></td>
+                                  <td><?=$row['password']?></td>
+                                  <td>
+                                    <a href="view.php?id=<?=$row['id']?>" class="btn btn-info">View</a>
+                                    <a href="edit.php?id=<?=$row['id']?>" class="btn btn-success">Edit</a>
+                                    <a href="delete.php?id=<?=$row['id']?>" class="btn btn-danger">Delete</a>
+                                  </td>
+                                </tr>
+                                <?php
+                              }
+                            }
+                            else{
+                              echo 'No users found!';
+                            }
+
+                          ?>
+                        </tbody>
+                      </table>
                     </div>
                 </div>
-
             </div>
-            
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
